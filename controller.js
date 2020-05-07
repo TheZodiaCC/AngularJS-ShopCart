4567b369ef779d4f807f51e3755c3ebeca9559dd
TheZodiaCC <53448802+TheZodiaCC@users.noreply.github.com>
2020-05-07 18:43:56 +0200

    1st Version

--- controller.js
+++ controller.js
@@ -1,19 +1,44 @@
 var app = angular.module("App", []);
 app.controller("appCtrl", function($scope) {
 
-    $scope.items = [];
+    $scope.items = [{name:"milk", ammount:1},
+    {name:"water", ammount:1}
+    ];
 
-    $scope.error = "";
+    $scope.addItem = function() {  
+        if($scope.item)
+        {
+            if (!$scope.findItem($scope.item)) 
+            {
+                $scope.newItem = {name:$scope.item, ammount:1};
+                $scope.items.push($scope.newItem);
+            }
+            else 
+            {
+                tempObj = $scope.findItemIndex($scope.item);
+                tempObj.ammount = tempObj.ammount + 1;
+            }
+        }      
+    }
 
-    $scope.addItem = function() {       
-        if ($scope.items.indexOf($scope.item) == -1) 
+    $scope.findItem = function(name) {
+        var result = $scope.items.find(n => n.name === name);
+
+        if(result)
         {
-            $scope.items.push($scope.item);
+            return true;
         }
-        else 
+        else
         {
-            
+            return false;
         }
+    }
+
+    $scope.findItemIndex = function(name)
+    {
+        tempObj = $scope.items.find(n => n.name === name);
+        console.log(tempObj);
+        return tempObj;
     }
 
     $scope.rmItem = function(index) {
